22b78dfd2070df5806520f50dd3e19a7f5befd53
Horacio H <hernandezh4@hotmail.com>
2024-03-05 17:00:33 -0800

    - to +

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
 alert('Esta es la suma de mis 2 numeros favoritos!!!');
 var num1 = 69;
 var num2 = 420;
-var sum = num1-num2;
+var sum = num1+num2;
 
 console.log('Este es mi numero favorito: ' + sum);
