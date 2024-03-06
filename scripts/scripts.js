256908615c06de7a0100948377faa2a83c1cda27
Horacio H <hernandezh4@hotmail.com>
2024-03-05 17:03:33 -0800

    Added third number to sum :D

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,8 @@
-alert('Esta es la suma de mis 2 numeros favoritos!!!');
+alert('Esta es la suma de mis 3 numeros favoritos!!!');
 var num1 = 69;
 var num2 = 420;
+var num3 = 12;
+var sum = num1+num2+num3;
 var sum = num1-num2;
 
 console.log('Este es mi numero favorito: ' + sum);
